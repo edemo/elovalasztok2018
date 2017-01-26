6bcf707be5913c7759012dccbd7df37016667c25
utopszkij <tibor.fogler@gmail.com>
2017-01-26 14:29:33 +0100

    unittest javitas

--- gitstart.bat
+++ gitstart.bat
@@ -3,8 +3,7 @@ REM git init
 REM fileok masolasa --> local repo
 REM cd local repo
 REM git inditasa
-XCOPY e:\www\valasztoimozgalom\elovalasztok\*.* e:\www\elovalasztok2018 /Y /s /e
-XCOPY e:\www\valasztoimozgalom\elovalasztok\tests\*.* e:\www\elovalasztok2018\tests /Y /s /e
+XCOPY e:\www\valasztoimozgalom\elovalasztok\*.* e:\www\elovalasztok2018 /Y /S /E
 cd e:\www\elovalasztok2018
 
 
